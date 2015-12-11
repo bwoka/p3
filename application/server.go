86c774575565c1e06a5094e3ed182ab2fcbfffbe
Brian A Wachowicz <bwachowi@ghc10.ghc.andrew.cmu.edu>
2015-12-11 15:14:28 -0500

    finished preliminary code for server,needs paxos

--- application/server.go
+++ application/server.go
@@ -1,60 +1,92 @@
+package main
 
 import (
     "fmt"
     "net/http"
     "strconv"
+    "strings"
 )
 
-struct TodoServer{
-    port string
+type TodoServer struct{
+    port string}
+
+type todoServer interface {
+    addHandler(w http.ResponseWriter, r *http.Request)
+    deleteHandler(w http.ResponseWriter, r *http.Request)
+    renderPage(w http.ResponseWriter, r *http.Request)
+    start(port string)
 }
 
-func main(){
-    server1:=ts{port: "8080"}
-    go server1.start(":8080")
-    server2:=ts{port: "8081"}
-    go server2.start(":8081")
-    server3:=ts{port: "8082"}
-    go server3.start(":8082")
-    server4:=ts{port: "8083"}
-    go server4.start(":8083")
-    paxos.add("topitem","0")
+
+func getPaxos(key string){
+
 }
 
-func (*todoServer ts) start(port string) {
+func setPaxos(key string,value string){
+
+}
+
+func (ts *TodoServer ) start(port string) {
     http.HandleFunc("/add", ts.addHandler)
     http.HandleFunc("/delete", ts.deleteHandler)
     http.HandleFunc("/", ts.renderPage)
     http.ListenAndServe(port, nil)
 }
 
-func getPaxosContent(url string)[]string{
-    last:= strconv.Atoi(getPaxos("topitem"))
-    content:=make([]sting, last+1)
+
+func (ts *TodoServer ) renderPage(w http.ResponseWriter, r *http.Request) {
+     header:="<!DOCTYPE html>\n<html>\n<body>\n<h1>Shared Todo List</h1>"
+     //todo: probably have to adjust this one
+     content:=getPaxosContent(r.URL.Path)
+     footer:="<form action=\"/delete\" method=\"GET\">New Item:<input type=\"text\" name=\"newItem\"></form>\n</body>\n</html>"
+     fmt.Fprintf(w, strings.Join([]string{header, content,footer},"\n"), r.URL.Path[1:])
+}
+
+
+func addtoPaxos(s string){
+    last,_:= strconv.Atoi(getPaxos("topitem"))
+    setPaxos(strconv.Itoa(last+1),s)
+}
+
+func deleteFromPaxos(s string){
+    setPaxos(s,"")
+}
+    
+
+
+
+func getPaxosContent(url string) string{
+    last,_:= strconv.Atoi(getPaxos("topitem"))
+    content:=make([]string, last+1)
     for x:=0; x<=last; x++{
-        xString=strconv.Itoa(x)
+        xString:=strconv.Itoa(x)
         item:=getPaxos(xString)
-        content[x]=strings.Join([]string{'<div><a href="',url,'delete?name1=value1&name2=value2">',item,"</div>\n"},"")
+        content[x]=strings.Join([]string{"<div><a href=\"",url,"delete?name1=value1&name2=value2\">",item,"</div>\n"},"")
     }
     return strings.Join(content,"")
 }
 
-func (*todoServer ts)  deleteHandler(w http.ResponseWriter, r *http.Request) {
+func (ts *TodoServer )  deleteHandler(w http.ResponseWriter, r *http.Request) {
     toDelete := r.Header.Get("toDelete")
     deleteFromPaxos(toDelete)
-    renderPage(w http.ResponseWriter, r *http.Request)
+    ts.renderPage(w, r)
 }
 
-func (*todoServer ts) addHandler(w http.ResponseWriter, r *http.Request) {
-    toDelete := r.Header.Get("newItem")
+func (ts *TodoServer ) addHandler(w http.ResponseWriter, r *http.Request) {
+    newItem := r.Header.Get("newItem")
     addtoPaxos(newItem)
-    renderPage(w http.ResponseWriter, r *http.Request)
+    ts.renderPage(w, r)
 }
 
-func (*todoServer ts) renderPage(w http.ResponseWriter, r *http.Request) {
-     header:="<!DOCTYPE html>\n<html>\n<body>\n<h1>Shared Todo List</h1>"
-     //todo: probably have to adjust this one
-     content:=getPaxosContent(r.Url.Path[0])
-     footer:='<form action="/delete" method="GET">New Item:<input type="text" name="newItem"></form>\n</body>\n</html>'
-     fmt.Fprintf(w, strings.Join([]string{header, content,footer},"\n"), r.URL.Path[1:])
+
+func main(){
+    server1:=TodoServer{port: "8080"}
+    go server1.start(":8080")
+    server2:=TodoServer{port: "8081"}
+    go server2.start(":8081")
+    server3:=TodoServer{port: "8082"}
+    go server3.start(":8082")
+    server4:=TodoServer{port: "8083"}
+    go server4.start(":8083")
+    setPaxos("topitem","0")
 }
